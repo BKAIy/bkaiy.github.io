24ffa95af072446914b3d7447575133d843077e3
BKFighter <bkfergen@gmail.com>
2016-11-02 18:28:24 -0500

    Update BKAIy.js

--- BKAIy.js
+++ BKAIy.js
@@ -1,5 +1,31 @@
+//SETS FUNCTION brain() TO A TIMER
 var brainTimer = setInterval(brain, 1000/20);
 
 function brain(){
   
+  //CYCLES THROUGH ALL NEURONS
+  for(var i=1;i<neurons.length;i++){
+    
+    //CHECKS TO SEE IF NEURON ISN'T RESERVED FOR INPUT (I.E. HAS CONNECTIONS)
+    if(neurons[i].con != undefined){
+      var stateWeight = 0;
+      
+      //FINDS WEIGHT OF ALL ACTIVATED NEURONS CONNECTED TO CURRENT NEURON
+      for(var a=0;a<neurons[i].con;a++){
+        if(neurons[neurons[i].con[a].index].state == true){
+         stateWeight = stateWeight + neurons[i].con[a].index; 
+        }
+      }
+      
+      //ASSIGNS STATE OF NEURON TO TRUE OR FALSE BASED ON WEIGHT
+      if(stateWeight >= neurons[i].act){
+       neurons[i].state = true; 
+      }else{
+       neurons[i].state = false; 
+      }
+    }
+    
+  }
+  
 };
+//END FUNCTION brain()
